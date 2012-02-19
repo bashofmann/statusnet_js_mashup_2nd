816b7a21b7ca11236abfc42638ac204854cd8388
Bastian Hofmann <bashofmann@googlemail.com>
2012-02-19 18:02:30 +0100

    local storage and logout

--- js/client.js
+++ js/client.js
@@ -14,17 +14,18 @@ var app = Sammy('#main', function() {
         var consumerKey = '71b454c797a58e8de5df33137e95cb8c';
         window.open('http://dev.status.net:8080/index.php/api/oauth2/authorize?response_toke=token&client_id=' + consumerKey, 'StatusNetLoginPopup', 'width=400&height=400');
     });
-    
     this.bind('loggedIn', function() {
+        this.redirect('/statusnet_js_mashup_2nd');
+    });
+    this.get('/statusnet_js_mashup_2nd/Logout', function() {
+        $('#logout-link').hide();
+        oauth2.deleteAccessToken();
         this.redirect('/statusnet_js_mashup_2nd');
     });
     
     this.bind('getFeed', function() {
         var that = this;
-        if (! oauth2.authParameters || ! oauth2.authParameters['access_token']) {
-            this.redirect('/statusnet_js_mashup_2nd/Login');
-            return;
-        } 
+        
         $.ajax({
           url: 'http://dev.status.net:8080/index.php/api/statuses/home_timeline.json?oauth_token=' + oauth2.authParameters['access_token'],
           success: function(response) {
@@ -33,6 +34,21 @@ var app = Sammy('#main', function() {
           }
         });
     });
+    
+    var checkLoggedIn = function(callback) {
+        if (this.path === '/statusnet_js_mashup_2nd/Login') {
+            callback();
+        }
+        if (! oauth2.isLoggedIn()) {
+            $('#logout-link').hide();
+            this.redirect('/statusnet_js_mashup_2nd/Login');
+        } else {
+            $('#logout-link').show();
+            callback();
+        }
+    };
+    
+    this.around(checkLoggedIn);
 });
 
 jQuery(function() {
@@ -50,6 +66,22 @@ var oauth2 = {
                 oauth2.authParameters[fragment[i].substr(0, ix)] = decodeURIComponent(fragment[i].substr(ix + 1));
             }
         }
+        localStorage.setItem("access_token", oauth2.authParameters['access_token']);
+        localStorage.setItem("user_id", oauth2.authParameters['user_id']);
         app.trigger('loggedIn');
+    },
+    isLoggedIn: function() {
+        return oauth2.authParameters['access_token'];
+    },
+    retrieveAccessToken: function() {
+        oauth2.authParameters['access_token'] = localStorage.getItem("access_token");
+        oauth2.authParameters['user_id'] = localStorage.getItem("user_id");
+    },
+    deleteAccessToken: function() {
+        oauth2.authParameters = {};
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("user_id");
     }
 };
+
+oauth2.retrieveAccessToken();
